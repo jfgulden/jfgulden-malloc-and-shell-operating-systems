dfa77fcacca8d373fe7ba85b2744c6ed5dc2d2ab
LucasAlda <lucas@aldazabal.com.ar>
2023-04-20 19:14:32 -0300

    history built-in implementado

--- shell/builtin.c
+++ shell/builtin.c
@@ -8,13 +8,10 @@ extern char prompt[PRMTLEN];
 // should be performed
 //
 // (It must not be called from here)
-int
-exit_shell(char *cmd)
-{
-	if (strcmp(cmd, "exit"))
-		return 0;
+int exit_shell(char *cmd) {
+    if (strcmp(cmd, "exit")) return 0;
 
-	return 1;
+    return 1;
 }
 
 // returns true if "chdir" was performed
@@ -29,30 +26,28 @@ exit_shell(char *cmd)
 // Examples:
 //  1. cmd = ['c','d', ' ', '/', 'b', 'i', 'n', '\0']
 //  2. cmd = ['c','d', '\0']
-int
-cd(char *cmd)
-{
-	if (strncmp(cmd, "cd", 2)) {
-		return 0;
-	}
+int cd(char *cmd) {
+    if (strncmp(cmd, "cd", 2)) {
+        return 0;
+    }
 
-	char *dir;
+    char *dir;
 
-	if (cmd[2] == '\0') {
-		dir = getenv("HOME");
-	} else if (cmd[2] == ' ') {
-		dir = cmd + 3;
-	} else {
-		return 0;
-	}
+    if (cmd[2] == '\0') {
+        dir = getenv("HOME");
+    } else if (cmd[2] == ' ') {
+        dir = cmd + 3;
+    } else {
+        return 0;
+    }
 
-	if (chdir(dir)) {
-		perror("Error al cambiar de directorio");
-	} else {
-		snprintf(prompt, sizeof prompt, "(%s)", getcwd(NULL, 0));
-	}
+    if (chdir(dir)) {
+        perror("Error al cambiar de directorio");
+    } else {
+        snprintf(prompt, sizeof prompt, "(%s)", getcwd(NULL, 0));
+    }
 
-	return 1;
+    return 1;
 }
 
 // returns true if 'pwd' was invoked
@@ -60,14 +55,12 @@ cd(char *cmd)
 //
 // (It has to be executed here and then
 // 	return true)
-int
-pwd(char *cmd)
-{
-	if (strcmp(cmd, "pwd")) {
-		return 0;
-	}
-	printf("%s\n", getcwd(NULL, 0));
-	return 1;
+int pwd(char *cmd) {
+    if (strcmp(cmd, "pwd")) {
+        return 0;
+    }
+    printf("%s\n", getcwd(NULL, 0));
+    return 1;
 }
 
 // returns true if `history` was invoked
@@ -75,10 +68,51 @@ pwd(char *cmd)
 //
 // (It has to be executed here and then
 // 	return true)
-int
-history(char *cmd)
-{
-	// Your code here
+int history(char *cmd) {
+    if (strncmp(cmd, "history", 7)) {
+        return 0;
+    }
 
-	return 0;
+    FILE *histfile;
+    char *histfile_dir = getenv("HISTFILE");
+    if (histfile_dir == NULL) {
+        histfile_dir = ".fisop_history";
+    }
+
+    histfile = fopen(histfile_dir, "r");
+
+    if (cmd[7] == '\0') {
+        char line[ARGSIZE];
+        while (fgets(line, ARGSIZE, histfile) != NULL) {
+            printf("%s", line);
+        }
+
+        return 1;
+    }
+
+    if (cmd[7] != ' ') {
+        return 0;
+    }
+
+    int n = atoi(cmd + 8);
+
+    char line[ARGSIZE];
+    fseek(histfile, 0, SEEK_END);
+    int pos = ftell(histfile);
+
+    int i = 0;
+    while (pos && i <= n) {
+        fseek(histfile, --pos, SEEK_SET);
+        if (fgetc(histfile) == '\n') i++;
+    }
+
+    if (pos == 0) {
+        fseek(histfile, 0, SEEK_SET);
+    }
+
+    while (fgets(line, ARGSIZE, histfile)) {
+        fprintf(stdout, "%s", line);
+    }
+
+    return 1;
 }
--- shell/runcmd.c
+++ shell/runcmd.c
@@ -3,62 +3,73 @@
 int status = 0;
 struct cmd *parsed_pipe;
 
+void write_command_history(char *cmd);
+
 // runs the command in 'cmd'
-int
-run_cmd(char *cmd)
-{
-	pid_t p;
-	struct cmd *parsed;
+int run_cmd(char *cmd) {
+    pid_t p;
+    struct cmd *parsed;
 
-	// if the "enter" key is pressed
-	// just print the prompt again
-	if (cmd[0] == END_STRING)
-		return 0;
+    write_command_history(cmd);
 
-	// "history" built-in call
-	if (history(cmd))
-		return 0;
+    // if the "enter" key is pressed
+    // just print the prompt again
+    if (cmd[0] == END_STRING) return 0;
 
-	// "cd" built-in call
-	if (cd(cmd))
-		return 0;
+    // "history" built-in call
+    if (history(cmd)) return 0;
 
-	// "exit" built-in call
-	if (exit_shell(cmd))
-		return EXIT_SHELL;
+    // "cd" built-in call
+    if (cd(cmd)) return 0;
 
-	// "pwd" built-in call
-	if (pwd(cmd))
-		return 0;
+    // "exit" built-in call
+    if (exit_shell(cmd)) return EXIT_SHELL;
 
+    // "pwd" built-in call
+    if (pwd(cmd)) return 0;
 
-	// parses the command line
-	parsed = parse_line(cmd);
+    // parses the command line
+    parsed = parse_line(cmd);
 
-	// forks and run the command
-	if ((p = fork()) == 0) {
-		// keep a reference
-		// to the parsed pipe cmd
-		// so it can be freed later
-		if (parsed->type == PIPE)
-			parsed_pipe = parsed;
+    // forks and run the command
+    if ((p = fork()) == 0) {
+        // keep a reference
+        // to the parsed pipe cmd
+        // so it can be freed later
+        if (parsed->type == PIPE) parsed_pipe = parsed;
 
-		exec_cmd(parsed);
-	}
+        exec_cmd(parsed);
+    }
 
-	// stores the pid of the process
-	parsed->pid = p;
+    // stores the pid of the process
+    parsed->pid = p;
 
-	waitpid(-1, &status, WNOHANG);
+    waitpid(-1, &status, WNOHANG);
 
-	if (parsed->type == BACK) {
-		print_back_info(parsed);
-	} else {
-		waitpid(p, &status, 0);
-		print_status_info(parsed);
-	}
+    if (parsed->type == BACK) {
+        print_back_info(parsed);
+    } else {
+        waitpid(p, &status, 0);
+        print_status_info(parsed);
+    }
 
-	free_command(parsed);
+    free_command(parsed);
 
-	return 0;
+    return 0;
+}
+
+void write_command_history(char *cmd) {
+    char *histfile_dir = getenv("HISTFILE");
+    if (histfile_dir == NULL) {
+        histfile_dir = ".fisop_history";
+    }
+
+    int histfile =
+        open(histfile_dir, O_RDWR | O_CREAT | O_APPEND, S_IWUSR | S_IRUSR);
+    if (histfile < 0) {
+        perror("error opening history file");
+        exit(-1);
+    }
+    write(histfile, cmd, strlen(cmd));
+    write(histfile, "\n", 1);
 }
